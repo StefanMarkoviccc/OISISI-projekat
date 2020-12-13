e89d06d6b5cfe7a56d02d5a5d444a1ea717d2878
VioletaKukic <75587782+VioletaKukic@users.noreply.github.com>
2020-12-13 18:04:59 +0100

    #dodat kod u klasi profesor

--- PROJEKAT-Studentska_sluzba/src/Model/Profesor.java
+++ PROJEKAT-Studentska_sluzba/src/Model/Profesor.java
@@ -1,5 +1,105 @@
 package Model;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Profesor {
+	
+	private String prezime;
+	private String ime;
+	private LocalDateTime datumR;
+	private String adresaStanovanja;
+	private String telefon;
+	private String email;
+	private String adresaKancelarije;
+	private String brLK;
+	private String titula;
+	private String zvanje;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public Profesor(String prezime, String ime, LocalDateTime datumR, String adresaStanovanja, String telefon,
+			String email, String adresaKancelarije, String brLK, String titula, String zvanje) {
+		super();
+		this.prezime = prezime;
+		this.ime = ime;
+		this.datumR = datumR;
+		this.adresaStanovanja = adresaStanovanja;
+		this.telefon = telefon;
+		this.email = email;
+		this.adresaKancelarije = adresaKancelarije;
+		this.brLK = brLK;
+		this.titula = titula;
+		this.zvanje = zvanje;
+		
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public LocalDateTime getDatumR() {
+		return datumR;
+	}
+	public void setDatumR(LocalDateTime datumR) {
+		this.datumR = datumR;
+	}
+	public String getAdresaStanovanja() {
+		return adresaStanovanja;
+	}
+	public void setAdresaStanovanja(String adresaStanovanja) {
+		this.adresaStanovanja = adresaStanovanja;
+	}
+	public String getTelefon() {
+		return telefon;
+	}
+	public void setTelefon(String telefon) {
+		this.telefon = telefon;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdresaKancelarije() {
+		return adresaKancelarije;
+	}
+	public void setAdresaKancelarije(String adresaKancelarije) {
+		this.adresaKancelarije = adresaKancelarije;
+	}
+	public String getBrLK() {
+		return brLK;
+	}
+	public void setBrLK(String brLK) {
+		this.brLK = brLK;
+	}
+	public String getTitula() {
+		return titula;
+	}
+	public void setTitula(String titula) {
+		this.titula = titula;
+	}
+	public String getZvanje() {
+		return zvanje;
+	}
+	public void setZvanje(String zvanje) {
+		this.zvanje = zvanje;
+	}
+	
+	
+	
 
 }
