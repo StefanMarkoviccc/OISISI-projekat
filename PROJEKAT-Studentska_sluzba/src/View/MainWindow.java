1237015fc5a21e9a81a4dd93b4788743d0dccb8a
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2020-12-16 19:15:48 +0100

    #main_window

--- PROJEKAT-Studentska_sluzba/src/View/MainWindow.java
+++ PROJEKAT-Studentska_sluzba/src/View/MainWindow.java
@@ -1,5 +1,37 @@
 package View;
 
-public class MainWindow {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+	
+	private StatusBar status;
+	private static MainWindow instance;
+
+	public static MainWindow getInstance() 
+	{
+		if(instance==null) 
+		{
+			instance=new MainWindow();
+		}
+		return instance;
+	}
+	
+	public MainWindow()
+	{
+		setPreferredSize(new Dimension(500, 500));
+		pack();
+		setJMenuBar(new MenuBar());
+		setLayout(new BorderLayout());
+		status = new StatusBar();
+		add(status,BorderLayout.SOUTH);
+		
+	}
+
+
+	
+	
+	
 }
