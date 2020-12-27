e436064887c4218d2a4c0a7faa7b556deba30105
VioletaKukic <75587782+VioletaKukic@users.noreply.github.com>
2020-12-27 17:59:53 +0100

    #toolbar

---
+++ DOMACI-2/src/ToolBar.java
@@ -0,0 +1,44 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ToolBar extends JPanel {
+	
+	private JButton btnDodaj;
+	private JButton btnEdit;
+	private JButton btnIzbrisi;
+	private JButton btnPretrazi;
+	private JTextField txtPretrazi;
+	
+	ToolBar()
+	{
+		setLayout(new BorderLayout());
+		JPanel left = new JPanel();
+		JPanel right = new JPanel();
+		
+		left.setLayout(new FlowLayout(FlowLayout.LEFT));
+		left.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		
+		btnDodaj = new JButton("Add");
+		btnEdit = new JButton("Edit");
+		btnIzbrisi = new JButton("Delete");
+		btnPretrazi = new JButton("Search");
+		txtPretrazi = new JTextField();
+		txtPretrazi.setPreferredSize(new Dimension(200,25));
+		
+		right.add(txtPretrazi);
+		right.add(btnPretrazi);
+		left.add(btnDodaj);
+		left.add(btnEdit);
+		left.add(btnIzbrisi);
+		
+		add(right,BorderLayout.EAST);
+		add(left,BorderLayout.WEST);
+	}
+	
+
+}
