0f23b3a6fa67e372dfd6a27953f32909511bb5f8
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2020-12-27 21:17:49 +0100

    #dodavanje_studenta dodavanje kontrolera za dugme odustani

---
+++ DOMACI-2/src/Kontroleri/CancelController.java
@@ -0,0 +1,24 @@
+package Kontroleri;
+
+
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.NewStudentWindow;
+
+public class CancelController extends AbstractAction {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		NewStudentWindow.getInstance().dispose();
+		
+	}
+
+	public CancelController() 
+	{
+		super("Odustani");
+	}
+}
