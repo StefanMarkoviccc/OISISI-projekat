8e47fff0a201e7bfb08eb02f9a50931589f0109f
VioletaKukic <75587782+VioletaKukic@users.noreply.github.com>
2020-12-27 18:07:52 +0100

    #glavni_prozor

--- DOMACI-2/src/MainWindow.java
+++ DOMACI-2/src/MainWindow.java
@@ -1,4 +1,36 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-public class MainWindow {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+
+public class MainWindow extends JFrame{
+
+	public WorkSpace workSpace;
+	public ToolBar toolBar;
+	public static MainWindow instance;
+	
+	public MainWindow() 
+	{
+		setPreferredSize(new Dimension(500,500));;
+		pack();
+		toolBar = new ToolBar();
+		workSpace = new WorkSpace();
+		add(toolBar,BorderLayout.NORTH);
+		add(workSpace,BorderLayout.CENTER);
+		
+		
+	}
+	
+	
+	
+	public static MainWindow getInstance() 
+	{
+		if(instance==null) 
+		{
+			instance=new MainWindow();
+		}
+		return instance;
+	}
 }
