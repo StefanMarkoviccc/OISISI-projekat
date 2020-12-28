d0a7f7be4d9a45c04251fa3f6883364970f5a944
VioletaKukic <75587782+VioletaKukic@users.noreply.github.com>
2020-12-28 02:29:47 +0100

    #333

--- PROJEKAT-Studentska_sluzba/src/Controllers/WindowListener.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/WindowListener.java
@@ -13,10 +13,14 @@ public class WindowListener extends WindowAdapter
 	
 	public void windowOpened(WindowEvent e) 
 	{
+<<<<<<< Updated upstream
 		DataSaveClass.Deserijalizacija( ".\\DataSave\\data.ss");
 		WorkSpace.insertTableStudent();
 		WorkSpace.insertTablePredmet();
 		WorkSpace.insertTableProfesor();
+=======
+		//DataSaveClass.Deserijalizacija( ".\\DataSave\\data.ss");		
+>>>>>>> Stashed changes
 	}
 	
 	public void windowClosing(WindowEvent e) 
