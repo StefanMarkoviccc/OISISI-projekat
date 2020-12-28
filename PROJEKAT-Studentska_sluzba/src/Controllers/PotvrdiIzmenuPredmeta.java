e6b2d93f195ce12b94d8e082fa772999ed8c3836
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2020-12-28 19:23:42 +0100

    #ll

--- PROJEKAT-Studentska_sluzba/src/Controllers/PotvrdiIzmenuPredmeta.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/PotvrdiIzmenuPredmeta.java
@@ -4,8 +4,10 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
+import Model.Predmet;
 import Model.Profesor;
 import View.MainWindow;
+import View.NewPredmetWindow;
 
 public class PotvrdiIzmenuPredmeta extends AbstractAction 
 {
@@ -16,7 +18,10 @@ public class PotvrdiIzmenuPredmeta extends AbstractAction
 		int row = MainWindow.getInstance().getWorkSpace().getTableProfesori().getSelectedRow();
 		Predmet pred= MainWindow.getInstance().getModel().getPredmeti().get(row);
 		
-		
+		String sifraPred=NewPredmetWindow.getInstance().getTxtSifraPred().getText();
+		String nazivPred=NewPredmetWindow.getInstance().getTxtSifraPred().getText();
+		String  brLik=NewPredmetWindow.getInstance().getTxtProfesor().getText(); 	
+		int ESPB=Integer.parseInt(NewPredmetWindow.getInstance().getTxtESPB().getText());
 	}
 
 }
