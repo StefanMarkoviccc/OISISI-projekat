6202ed7de76a161df9effe1adf32ef4d94ae2af0
VioletaKukic <75587782+VioletaKukic@users.noreply.github.com>
2020-12-28 02:15:31 +0100

    #edit

---
+++ PROJEKAT-Studentska_sluzba/src/Controllers/EditEntityControler.java
@@ -0,0 +1,15 @@
+package Controllers;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class EditEntityControler extends AbstractAction {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		
+	}
+
+}
