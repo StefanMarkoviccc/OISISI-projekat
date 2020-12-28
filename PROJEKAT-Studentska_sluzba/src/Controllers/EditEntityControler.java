40a7f35b1af69e8235272eeffaa8573a63ffc67c
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2020-12-28 03:38:16 +0100

    #izmena_studenta

--- PROJEKAT-Studentska_sluzba/src/Controllers/EditEntityControler.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/EditEntityControler.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
+import Model.Student;
 import View.EditProfesorWindow;
 import View.EditStudentWindow;
 import View.MainWindow;
@@ -21,7 +22,19 @@ public class EditEntityControler extends AbstractAction {
 		int indx=MainWindow.getInstance().getWorkSpace().getTabs().getSelectedIndex();
 		if(indx==0) 
 		{
+			int row=MainWindow.getInstance().getWorkSpace().getTableStudenti().getSelectedRow();
+			Student stud=MainWindow.getInstance().getModel().getStudenti().get(row);
+			
+			EditStudentWindow.getInstance().getTxtAdresaStan().setText(stud.getAdresaStanovanja());
+			EditStudentWindow.getInstance().getTxtIme().setText(stud.getIme());
+			EditStudentWindow.getInstance().getTxtBrIndexa().setText(stud.getBrIndexa());
+			EditStudentWindow.getInstance().getTxtBrTel().setText(stud.getKontaktTel());
+			EditStudentWindow.getInstance().getTxtDatumR().setText(stud.getDatumR().toString());
+			EditStudentWindow.getInstance().getTxtMail().setText(stud.getEmailAdresa());
+			EditStudentWindow.getInstance().getTxtPrezime().setText(stud.getPrezime());
+			EditStudentWindow.getInstance().getTxtGodUpisa().setText(String.valueOf(stud.getGodUpisa()));
 			EditStudentWindow.getInstance().show();
+			
 		}
 		else if(indx==2) 
 		{
