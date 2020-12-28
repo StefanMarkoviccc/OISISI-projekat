18127b120245c9869dc9fe6f27abc722dd583e4b
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2020-12-28 03:27:03 +0100

    #izmena_studenta

---
+++ PROJEKAT-Studentska_sluzba/src/Controllers/PotvrdiIzmenuStudenta.java
@@ -0,0 +1,16 @@
+package Controllers;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class PotvrdiIzmenuStudenta extends AbstractAction
+{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0)
+	{
+			
+	}
+
+}
