37989fe229156626419fb473de6c45a4238aa3e0
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-01-05 20:29:47 +0100

    #obrisi_entitet

--- PROJEKAT-Studentska_sluzba/src/Controllers/DeleteEntityController.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/DeleteEntityController.java
@@ -25,7 +25,6 @@ public class DeleteEntityController extends AbstractAction
 			if(index!=-1) 
 			{
 				DeleteEntityWindow.getInstance("studenta").show();
-				
 			}
 		}
 		else if(tabbedIndex==1) 
