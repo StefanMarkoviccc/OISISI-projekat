e2dce559396c765d968b2660129ecb2f0db4a1ed
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-01-11 16:01:45 +0100

    #pretraga_studenata

--- PROJEKAT-Studentska_sluzba/src/Controllers/ButtonSearchController.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/ButtonSearchController.java
@@ -1,5 +1,34 @@
 package Controllers;
 
-public class ButtonSearchController {
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.MainWindow;
+import View.WorkSpace;
+
+public class ButtonSearchController extends AbstractAction {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		int index = MainWindow.getInstance().getWorkSpace().getTabs().getSelectedIndex();
+		
+		if(index == 0) 
+		{
+			String s = MainWindow.getInstance().getTool().getTxtSearch().getText();
+			String[] parts = s.split(" ");
+			System.out.println(parts.length);
+			
+		};
+		
+		
+		
+	}
+	
+	public ButtonSearchController() 
+	{
+		super("Search");
+	}
 
 }
--- PROJEKAT-Studentska_sluzba/src/View/ToolBar.java
+++ PROJEKAT-Studentska_sluzba/src/View/ToolBar.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import Controllers.ButtonSearchController;
 import Controllers.DeleteEntityController;
 import Controllers.EditEntityControler;
 import Controllers.NewEntityController;
@@ -38,7 +39,7 @@ public class ToolBar extends JPanel {
 		btnAdd.setText("+");
 		btnEdit = new JButton(new EditEntityControler());
 		btnDelete = new JButton(new DeleteEntityController());
-		btnSearch = new JButton("Search");
+		btnSearch = new JButton(new ButtonSearchController());
 		txtSearch = new JTextField();
 		txtSearch.setPreferredSize(new Dimension(200,28));
 		
@@ -53,6 +54,46 @@ public class ToolBar extends JPanel {
 		
 		
 		
+	}
+
+	public JButton getBtnAdd() {
+		return btnAdd;
+	}
+
+	public void setBtnAdd(JButton btnAdd) {
+		this.btnAdd = btnAdd;
+	}
+
+	public JButton getBtnEdit() {
+		return btnEdit;
+	}
+
+	public void setBtnEdit(JButton btnEdit) {
+		this.btnEdit = btnEdit;
+	}
+
+	public JButton getBtnDelete() {
+		return btnDelete;
+	}
+
+	public void setBtnDelete(JButton btnDelete) {
+		this.btnDelete = btnDelete;
+	}
+
+	public JButton getBtnSearch() {
+		return btnSearch;
+	}
+
+	public void setBtnSearch(JButton btnSearch) {
+		this.btnSearch = btnSearch;
+	}
+
+	public JTextField getTxtSearch() {
+		return txtSearch;
+	}
+
+	public void setTxtSearch(JTextField txtSearch) {
+		this.txtSearch = txtSearch;
 	}
 	
 	
