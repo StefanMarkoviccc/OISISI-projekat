21ca5865a802b81284f048a5ee1af13327058921
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-01-12 18:14:04 +0100

    #uklanjanje_predmeta_sa_studenta

---
+++ PROJEKAT-Studentska_sluzba/src/Controllers/UklanjanjePredmetaStudentuController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import View.UklanjanjePredmetaStudentuWindow;
+
+public class UklanjanjePredmetaStudentuController extends AbstractAction{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		UklanjanjePredmetaStudentuWindow.getInstance().show();
+		
+	}
+
+}
