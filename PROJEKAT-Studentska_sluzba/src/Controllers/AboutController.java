e641c3e066b51d70ebe58f2547f6c88605e4f677
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-01-13 17:04:47 +0100

    #menu_bar prosiren About i Help

--- PROJEKAT-Studentska_sluzba/src/Controllers/AboutController.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/AboutController.java
@@ -15,7 +15,7 @@ public class AboutController extends AbstractAction {
 	public void actionPerformed(ActionEvent arg0) {
 		
 		JDialog dial = new JDialog(MainWindow.getInstance(),"About");
-		JLabel lbl = new JLabel("This is About");
+		JLabel lbl = new JLabel("Program namenjen za rad studentske sluzbe koji sluzi za pomoc organizacije podataka.");
 		dial.setPreferredSize(new Dimension(200,200));
 		dial.pack();
 		dial.add(lbl);
--- PROJEKAT-Studentska_sluzba/src/Controllers/HelpController.java
+++ PROJEKAT-Studentska_sluzba/src/Controllers/HelpController.java
@@ -20,14 +20,15 @@ public class HelpController extends AbstractAction{
 		JDialog dial = new JDialog(MainWindow.getInstance(),"Help");
 		dial.setLayout(new BorderLayout());
 		
-		JLabel label1 = new JLabel("Akceleratori: ");
-		JLabel label2 = new JLabel("CTRL-H Help:");
-		JLabel label3 = new JLabel("CTRL-N Dodaj novi entitet:");
-		JLabel label4 = new JLabel("CTRL-A Prozor about:");
-		JLabel label5 = new JLabel("H Help prozor:");
-		JLabel label6 = new JLabel("N Novi entitet prozor:");
-		JLabel label7 = new JLabel("A About prozor:");
+		JLabel label1 = new JLabel("Akceleratori:  ");
+		JLabel label2 = new JLabel("CTRL-H Help prozor.");
+		JLabel label3 = new JLabel("CTRL-N Dodaj novi entitet.");
+		JLabel label4 = new JLabel("CTRL-A Prozor about.");
+		JLabel label5 = new JLabel("H Help prozor.");
+		JLabel label6 = new JLabel("N Novi entitet prozor.");
+		JLabel label7 = new JLabel("A About prozor.");
 		JLabel label8 = new JLabel("Mnemonici:");
+		JLabel label9 = new JLabel("Foramt datuma se unosi na sledeci nacin: 'yyyy-mm-dd' ");
 		
 		JPanel pnl = new JPanel();
 		pnl.setLayout(new BoxLayout(pnl, BoxLayout.Y_AXIS));
@@ -36,12 +37,13 @@ public class HelpController extends AbstractAction{
 		pnl.add(label2);
 		pnl.add(label3);
 		pnl.add(label4);
+		pnl.add(label8);
 		pnl.add(label5);
 		pnl.add(label6);
 		pnl.add(label7);
-		pnl.add(label8);
+		pnl.add(label9);
 		
-		dial.setPreferredSize(new Dimension(200,200));
+		dial.setPreferredSize(new Dimension(500,200));
 		dial.pack();
 		dial.add(pnl,BorderLayout.CENTER);
 		dial.show();
