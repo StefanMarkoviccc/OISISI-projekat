cc083978127c2c5d13f5d3e2838401ca37c9e593
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2021-01-13 22:28:01 +0100

    #menu_bar ikone

--- PROJEKAT-Studentska_sluzba/src/DataSave/Configuration.java
+++ PROJEKAT-Studentska_sluzba/src/DataSave/Configuration.java
@@ -1,10 +1,30 @@
 package DataSave;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 public class Configuration
 {
-	public static File file = new File("resources/modelData.ss");
-	public static String absolutePath = file.getAbsolutePath();
+	private static Path relativePath=Paths.get("");
+	public static String resourcesPath=relativePath.toAbsolutePath().toString()+"/resources/";
+	
+	
+	private static Path currentRelativePath= Paths.get("");
+
+	
+	public static Icon addIcon=  new ImageIcon(currentRelativePath.toAbsolutePath().toString()+"/slike/add.png");
+	public static Icon editIcon=  new ImageIcon(currentRelativePath.toAbsolutePath().toString()+"/slike/edit.png");
+	public static Icon deleteIcon=  new ImageIcon(currentRelativePath.toAbsolutePath().toString()+"/slike/bin.png");
+
+	public static Icon pretragaIcon= new ImageIcon(currentRelativePath.toAbsolutePath().toString()+"/slike/pretraga.png");
+
+	public static Icon closeIcon=  new ImageIcon(currentRelativePath.toAbsolutePath().toString()+"/slike/close1_3_13.png");
+
+	
+	
 
 }
